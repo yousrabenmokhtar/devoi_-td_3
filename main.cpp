19ea39b40f9eec0d441234a7ad98a0a0262079c6
yousrabenmokhtar <147003414+yousrabenmokhtar@users.noreply.github.com>
2023-11-04 19:25:35 +0100

    td_3_exercice7

--- main.cpp
+++ main.cpp
@@ -1,32 +1,39 @@
 #include <iostream>
-using namespace std;
-class erreur
-{ public :
-int num ;
-} ;
+using namespace std ;
 class A
-{ public :
-A(int n)
-{ if (n==1) { erreur er ; er.num = 999 ; throw er ; }
+{ int n ;
+float x ;
+public :
+A (int p = 2)
+{ n = p ; x = 1 ;
+cout << "** construction objet A : " << n << " " << x << "\n" ;
 }
 } ;
-//Quels résultats fournira ce programme utilisant ces deux classes :
-
+class B
+{ int n ;
+float y ;
+public :
+B (float v = 0.0)
+{ n = 1 ; y = v ;
+cout << "** construction objet B : " << n << " " << y << "\n" ;
+}
+} ;
+class C : public B, public A
+{ int n ;
+int p ;
+public :
+C (int n1=1, int n2=2, int n3=3, float v=0.0) : A (n1), B(v)
+{ n = n3 ; p = n1+n2 ;
+cout << "** construction objet C : " << n << " " << p <<"\n" ;
+}
+} ;
 main()
-{ void f() ;
-try
-{ f() ;
-}
-catch (erreur er)
-{ cout << "dans main : " << er.num << "\n" ;
-}
-cout << "suite main\n" ;
-} void f()
-{ try
-{ A a(1) ;
-}
-catch (erreur er)
-{ cout << "dans f : " << er.num << "\n"; }}
-  /*les resultats sont :
-    dans f : 999
-     suite main*/
+{ C c1 ;
+C c2 (10, 11, 12, 5.0) ;
+} /* ** construction objet B : 1 0
+** construction objet A : 1 1
+** construction objet C : 3 3
+** construction objet B : 1 5
+** construction objet A : 10 1
+** construction objet C : 12 21 */
+
